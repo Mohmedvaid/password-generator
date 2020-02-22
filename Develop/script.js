cee94532a1c43f8aad7443ccb8c4724a31039768
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-02-21 21:46:27 -0600

    Logic is working perfectly but the text is not getting displayed on the screen

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,93 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var AlphabetsArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','@', '#', '$', '%', '!','+', '-', '&', '|', '!', '(', ')', '{', '}', '[', ']', '^', '~', '*', '?', ':', ';','1', '2', '3','4', '5', '6', '7', '8', '9', '0'];
+
+//Initail declarations
+var LowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var UpperCase= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var SpecialChar= ['@', '#', '$', '%', '!','+', '-', '&', '|', '!', '(', ')', '{', '}', '[', ']', '^', '~', '*', '?', ':', ';'];
+var Numbers= ['1', '2', '3','4', '5', '6', '7', '8', '9', '0'];
+var rmd='';
+var FinalPass=[];
+var FinalString='';
 
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-  function generatePassword() {
+  //User inputs
     var length = prompt("how long you want the password to be? (Between 8 to 128)");
-
     var lowercase = confirm(`Do you want to include lowercase characters?`);
     var uppercase = confirm(`Do you want to include uppercase characters?`);
     var special = confirm(`Do you want to include special characters?`);
     var numbers = confirm(`Do you want to include numbers?`);
-    var ArrLength = '';
-    var rmd='';
-    var Finalpass= [];
-
-
-  //Code below will assign a random value to rmd var  
-    if(lowercase && uppercase && special && numbers){
-      rmd= Math.floor(Math.random() * 83);
-    }
-    else if(lowercase && uppercase && special ){
-      rmd= Math.floor(Math.random() * 73);
-    }
-    else if(lowercase && uppercase ){
-      rmd= Math.floor(Math.random() * 51);
-    }
-    else {
-      rmd= Math.floor(Math.random() * 25);
-    }
-
+    
+    //Main Loop
     for(var i=0; i<length; i++){
-      temp= AlphabetsArr[Math.floor(Math.random() * rmd)];
-      Finalpass.push(temp);
-    }
-    //Finalpass= Finalpass.toString();
-    console.log(Finalpass.join(''));
-    return Finalpass;
-    }
-  
 
+      //if lowercase is true FinalPass will include lower case chars
+      if(lowercase){
+        FinalPass.push(LowerCase[Math.floor(Math.random()*LowerCase.length-1)])
+      }
+
+      //if uppercase is true FinalPass will include upper case chars
+      if(uppercase){
+        FinalPass.push(UpperCase[Math.floor(Math.random()*UpperCase.length-1)])
+      }
+
+      //if special is true FinalPass will include Special chars
+      if(special){
+        FinalPass.push(SpecialChar[Math.floor(Math.random()*SpecialChar.length-1)])
+      }
+
+      //if numbers is true FinalPass will include numbers
+      if(numbers){
+        FinalPass.push(Numbers[Math.floor(Math.random()*Numbers.length-1)])
+      }
+    }
+    //Outsite of the Main loop
+    //Converting array to string
+      FinalString=FinalPass.join('');
+      console.log(FinalString);
+      return FinalString;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//OLD CODE
+// function generatePassword() {
+//   var length = prompt("how long you want the password to be? (Between 8 to 128)");
+
+//   var lowercase = confirm(`Do you want to include lowercase characters?`);
+//   var uppercase = confirm(`Do you want to include uppercase characters?`);
+//   var special = confirm(`Do you want to include special characters?`);
+//   var numbers = confirm(`Do you want to include numbers?`);
+//   var ArrLength = '';
+//   var rmd='';
+//   var Finalpass= [];
+
+
+// //Code below will assign a random value to rmd var  
+//   if(lowercase && uppercase && special && numbers){
+//     rmd= Math.floor(Math.random() * 83);
+//   }
+//   else if(lowercase && uppercase && special ){
+//     rmd= Math.floor(Math.random() * 73);
+//   }
+//   else if(lowercase && uppercase ){
+//     rmd= Math.floor(Math.random() * 51);
+//   }
+//   else {
+//     rmd= Math.floor(Math.random() * 25);
+//   }
+
+//   for(var i=0; i<length; i++){
+//     temp= AlphabetsArr[Math.floor(Math.random() * rmd)];
+//     Finalpass.push(temp);
+//   }
+//   //Finalpass= Finalpass.toString();
+//   console.log(Finalpass.join(''));
+//   return Finalpass;
+//   }
+
+// var LowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','@', '#', '$', '%', '!','+', '-', '&', '|', '!', '(', ')', '{', '}', '[', ']', '^', '~', '*', '?', ':', ';','1', '2', '3','4', '5', '6', '7', '8', '9', '0'];
