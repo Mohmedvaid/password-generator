00cb3ff41b494610ca7c0cf4c1e737fb38fdd9e4
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-02-23 12:45:10 -0600

    Revert "pushing final logic. Password still does not appear in the html"
    This reverts commit b92d8dddce48a178c6e8372c799f5a63b1a1c898.

--- Develop/script.js
+++ Develop/script.js
@@ -60,3 +60,40 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+//OLD CODE
+// function generatePassword() {
+//   var length = prompt("how long you want the password to be? (Between 8 to 128)");
+
+//   var lowercase = confirm(`Do you want to include lowercase characters?`);
+//   var uppercase = confirm(`Do you want to include uppercase characters?`);
+//   var special = confirm(`Do you want to include special characters?`);
+//   var numbers = confirm(`Do you want to include numbers?`);
+//   var ArrLength = '';
+//   var rmd='';
+//   var Finalpass= [];
+
+
+// //Code below will assign a random value to rmd var  
+//   if(lowercase && uppercase && special && numbers){
+//     rmd= Math.floor(Math.random() * 83);
+//   }
+//   else if(lowercase && uppercase && special ){
+//     rmd= Math.floor(Math.random() * 73);
+//   }
+//   else if(lowercase && uppercase ){
+//     rmd= Math.floor(Math.random() * 51);
+//   }
+//   else {
+//     rmd= Math.floor(Math.random() * 25);
+//   }
+
+//   for(var i=0; i<length; i++){
+//     temp= AlphabetsArr[Math.floor(Math.random() * rmd)];
+//     Finalpass.push(temp);
+//   }
+//   //Finalpass= Finalpass.toString();
+//   console.log(Finalpass.join(''));
+//   return Finalpass;
+//   }
+
+// var LowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','@', '#', '$', '%', '!','+', '-', '&', '|', '!', '(', ')', '{', '}', '[', ']', '^', '~', '*', '?', ':', ';','1', '2', '3','4', '5', '6', '7', '8', '9', '0'];
